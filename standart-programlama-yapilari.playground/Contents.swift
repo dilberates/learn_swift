04697dfc5197fb7137e8acd96f16d323b68b717e
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-05-31 10:06:44 +0300

    Completed
    if-while-for-switch

--- standart-programlama-yapilari.playground/Contents.swift
+++ standart-programlama-yapilari.playground/Contents.swift
@@ -16,4 +16,54 @@ print(s3>s1 && s1>s2) //ve
 print(s2<s3 || s1>s2) //veya
 
 //IF YAPISI
+var num1=30
+var num2=50
 
+num1==num2 ? print("num1 num2'ye eşittir") : print("num1 eşit değildir num2 ye") //Ifin kısa kullanımına bir örnek
+
+if num2 > 73{
+    num1=40
+}else if num1>10{
+    num1=20
+}else{
+    num2=30
+}
+
+//SWITCH YAPISI
+
+var girdi=40
+
+switch girdi{
+case 10:
+    print("Sayımız 10")
+case 20:
+    print("Sayımız 20")
+case 30:
+    print("Sayımız 30")
+case 40:
+    print("Sayımız 40")
+default:
+    print("Sayı ddeğeri 10 ile 40 aralığında değil.")
+}
+
+//FOR-WHILE
+
+//Bazı kodları tekrarlı çalıştırmak istediğimizde kullanırız.
+//For: kaç kez çalışacağı belli ise
+//While: bir koşula göre çalışır.
+
+// 1...5 : 1,2,3,4,5
+// 1..<5 : 1,2,3,4
+
+var dizi : [Int] = [10,20,30]
+
+for sayi in dizi{
+    print("Sayı: \(sayi)")
+}
+
+var a=6
+
+while a<10{
+    print(a)
+    a+=1
+}
